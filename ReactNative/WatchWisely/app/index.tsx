a38e8038d34f1fb00af151764868297535b9e647
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-10-29 16:49:45 +0530

    chore: Clean up project structure by removing unused .idea files and updating .gitignore; add About page and global styles

--- ReactNative/WatchWisely/app/index.tsx
+++ ReactNative/WatchWisely/app/index.tsx
@@ -1,15 +1,11 @@
 import {Text, View} from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Welcome!</Text>
+        <View className="flex-1 justify-center items-center">
+            <Text className="font-bold text-4xl text-black italic">Welcome!</Text>
+            <Link href="//About">About</Link>
         </View>
     );
 }
