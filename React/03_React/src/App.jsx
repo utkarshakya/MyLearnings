6ae9c508474fd0806602ea08372db9fe458fe145
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-10-29 11:29:16 +0530

    Add: Added the Counter in the App

--- React/03_React/src/App.jsx
+++ React/03_React/src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Body from "./Components/Body";
 import Header from "./Components/Header";
+import Counter from "./Components/Counter";
 
 function App() {
-    return (
-      <>
-        <Header></Header>
-        <Body></Body>
-      </>
-    );
-  }
+  return (
+    <>
+      <Header></Header>
+      <Body></Body>
+      <Counter />
+    </>
+  );
+}
 
 export default App;
