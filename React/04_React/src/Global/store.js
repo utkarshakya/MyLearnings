deb3d682a32e27606e113ef32628ffa5e5bd0f00
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-11-07 16:02:41 +0530

    style: format code for consistency; adjust spacing and quotes in store.js and user.js

--- React/04_React/src/Global/store.js
+++ React/04_React/src/Global/store.js
@@ -1,8 +1,8 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './user'
 
 const store = configureStore({
-    reducer:{
+    reducer: {
         users: userSlice
     }
 })
--- React/04_React/src/Global/user.js
+++ React/04_React/src/Global/user.js
@@ -1,35 +1,37 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchUserData = createAsyncThunk(
-    'users/fetchUserData',
+    "users/fetchUserData",
     async (howMany = 10, thunkAPI) => {
-        const response = await fetch(`https://dummyjson.com/user/?limit=${howMany}`)
-        return response.json()
+        const response = await fetch(
+            `https://dummyjson.com/user/?limit=${howMany}`
+        );
+        return response.json();
     }
-)
+);
 
 const userSlice = createSlice({
-    name: 'users',
+    name: "users",
     initialState: {
         data: null,
-        status: 'idle',
+        status: "idle",
         error: null,
     },
     reducers: {},
     extraReducers: (builder) => {
         builder
             .addCase(fetchUserData.pending, (state) => {
-                state.status = 'loading'
+                state.status = "loading";
             })
             .addCase(fetchUserData.fulfilled, (state, action) => {
-                state.status = 'succeeded'
-                state.data = action.payload
+                state.status = "succeeded";
+                state.data = action.payload;
             })
             .addCase(fetchUserData.rejected, (state, action) => {
-                state.status = 'failed'
-                state.error = action.error.message
-            })
-    }
-})
+                state.status = "failed";
+                state.error = action.error.message;
+            });
+    },
+});
 
-export default userSlice.reducer
+export default userSlice.reducer;
