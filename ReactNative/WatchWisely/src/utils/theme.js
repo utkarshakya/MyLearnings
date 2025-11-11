043f3b053ee02c94d43e64cc189c59880ff72f9a
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-11-11 18:27:58 +0530

    Move folder for better folder structure

---
+++ ReactNative/WatchWisely/src/utils/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    dark: {
+        background: "#1f2937",
+        text: "#FFFFFF",
+        primary: "#1F80E0",
+        secondary: "#03DAC6",
+    },
+    light: {
+        background: "#FFFFFF",
+        text: "#000000",
+        primary: "#1F80E0",
+        secondary: "#03DAC6",
+    }
+};
